fd0e860e476c87fb3d426fccd935b7a83fa5cf96
ckxsl023 <70303889+ckxsl023@users.noreply.github.com>
2022-05-14 11:44:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 29,
+      density: { enable: true, value_area: 1122.388442605866 }
+    },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "triangle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 4 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.352750653390415,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 4.3852169464272635,
+        opacity_min: 0.04060386061506725,
+        sync: false
+      }
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 288.6141709557941,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1.4430708547789706
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "top",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 1042.21783956259,
+        rotateY: 962.0472365193136
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: false, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
